8c9c73db9953a761438fe49258f12808e0e042dd
sumithak2006-hash <sumitha.k2006@gmail.com>
2026-02-11 21:45:56 +0530

    Add files via upload

---
+++ DivisionException.java
@@ -0,0 +1,10 @@
+class DivisionException{
+ public static void main(String[] args){
+  try{
+   int a=10,b=0;
+   System.out.println(a/b);
+  }catch(ArithmeticException e){
+   System.out.println("Cannot divide by zero");
+  }
+ }
+}
